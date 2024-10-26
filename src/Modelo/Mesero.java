5798b1daf9d9e6b5ba1b5354d945a771e2d7aed2
AlecFrame <walteravertacnik@sanluis.edu.ar>
2024-10-26 16:20:19 -0300

    Volviendo a una copia de seguridad
    Volviendo a una copia de seguridad

---
+++ src/Modelo/Mesero.java
@@ -0,0 +1,78 @@
+
+package Modelo;
+
+public class Mesero {
+    private int dniMesero;
+    private String nombre;
+    private String apellido;
+    private String reemplazando;
+    private boolean estado;
+
+    
+    
+
+    public Mesero(int dniMesero, String nombre, String apellido, String reemplazando, boolean estado) {
+        this.dniMesero = dniMesero;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.reemplazando = reemplazando;
+        this.estado = estado;
+    }
+
+    public Mesero(int aInt, String string, String string0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Mesero() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getDniMesero() {
+        return dniMesero;
+    }
+
+    public void setDniMesero(int dniMesero) {
+        this.dniMesero = dniMesero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getReemplazando() {
+        return reemplazando;
+    }
+
+    public void setReemplazando(String reemplazando) {
+        this.reemplazando = reemplazando;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    @Override
+    public String toString() {
+        return "Mesero{" +
+                "dniMesero=" + dniMesero +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                '}';
+    }
+}
---
+++ src/Vistas/VentPrincipal.java
@@ -0,0 +1,193 @@
+package Vistas;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.swing.JDesktopPane;
+import javax.swing.JOptionPane;
+
+
+public class VentPrincipal extends javax.swing.JFrame {
+
+   private EscritorioPersonalizado escritorio;
+   
+    public VentPrincipal() {
+        initComponents();
+        
+        escritorio= new VentPrincipal.EscritorioPersonalizado();
+        this.setContentPane(escritorio);
+        this.setLocationRelativeTo(this);
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jDesktopPane1 = new javax.swing.JDesktopPane();
+        jMenu = new javax.swing.JMenuBar();
+        jDetPedido = new javax.swing.JMenu();
+        jPedido = new javax.swing.JMenuItem();
+        jProductos = new javax.swing.JMenu();
+        jCategoria = new javax.swing.JMenuItem();
+        jReserva = new javax.swing.JMenu();
+        jMesero = new javax.swing.JMenu();
+        jSalir = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Menú");
+        setBackground(new java.awt.Color(255, 255, 204));
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        jDesktopPane1.setMinimumSize(new java.awt.Dimension(1, 10));
+
+        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
+        jDesktopPane1.setLayout(jDesktopPane1Layout);
+        jDesktopPane1Layout.setHorizontalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 573, Short.MAX_VALUE)
+        );
+        jDesktopPane1Layout.setVerticalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 404, Short.MAX_VALUE)
+        );
+
+        jMenu.setBackground(new java.awt.Color(152, 102, 0));
+        jMenu.setBorder(null);
+        jMenu.setForeground(new java.awt.Color(153, 102, 0));
+        jMenu.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jMenu.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jMenu.setInheritsPopupMenu(true);
+        jMenu.setMargin(new java.awt.Insets(2, 2, 2, 2));
+
+        jDetPedido.setBorder(null);
+        jDetPedido.setForeground(new java.awt.Color(255, 255, 204));
+        jDetPedido.setText("Detalle de Pedidos");
+
+        jPedido.setBackground(new java.awt.Color(204, 102, 0));
+        jPedido.setForeground(new java.awt.Color(255, 255, 204));
+        jPedido.setText("Pedido");
+        jPedido.setBorder(null);
+        jPedido.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jPedidoActionPerformed(evt);
+            }
+        });
+        jDetPedido.add(jPedido);
+
+        jMenu.add(jDetPedido);
+
+        jProductos.setBackground(new java.awt.Color(153, 102, 0));
+        jProductos.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(153, 102, 0)));
+        jProductos.setForeground(new java.awt.Color(255, 255, 204));
+        jProductos.setText("Productos");
+
+        jCategoria.setBackground(new java.awt.Color(204, 102, 0));
+        jCategoria.setForeground(new java.awt.Color(255, 255, 204));
+        jCategoria.setText("Categoría");
+        jCategoria.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(204, 153, 0)));
+        jCategoria.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCategoriaActionPerformed(evt);
+            }
+        });
+        jProductos.add(jCategoria);
+
+        jMenu.add(jProductos);
+
+        jReserva.setBackground(new java.awt.Color(153, 102, 0));
+        jReserva.setForeground(new java.awt.Color(255, 255, 204));
+        jReserva.setText("Reserva");
+        jMenu.add(jReserva);
+
+        jMesero.setBackground(new java.awt.Color(153, 102, 0));
+        jMesero.setForeground(new java.awt.Color(255, 255, 204));
+        jMesero.setText("Mesero");
+        jMenu.add(jMesero);
+
+        jSalir.setBorder(null);
+        jSalir.setForeground(new java.awt.Color(255, 255, 255));
+        jSalir.setText("Salir");
+        jSalir.setDoubleBuffered(true);
+        jSalir.setInheritsPopupMenu(true);
+        jSalir.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jSalirMouseClicked(evt);
+            }
+        });
+        jMenu.add(jSalir);
+
+        setJMenuBar(jMenu);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jDesktopPane1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jDesktopPane1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jPedidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPedidoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jPedidoActionPerformed
+
+    private void jSalirMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jSalirMouseClicked
+
+        int cartel= JOptionPane.showConfirmDialog(this, "¿Desea salir de la aplicación?","Precaución",JOptionPane.WARNING_MESSAGE);
+        
+        if(cartel == JOptionPane.YES_OPTION){
+            System.exit(WIDTH);
+        }
+        
+        
+   
+    }//GEN-LAST:event_jSalirMouseClicked
+
+    private void jCategoriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCategoriaActionPerformed
+        escritorio.removeAll();
+        escritorio.repaint();
+        VProducto v = new VProducto();
+        v.setVisible(true);
+        escritorio.add(v);
+        escritorio.moveToFront(v);
+    }//GEN-LAST:event_jCategoriaActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem jCategoria;
+    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JMenu jDetPedido;
+    private javax.swing.JMenuBar jMenu;
+    private javax.swing.JMenu jMesero;
+    private javax.swing.JMenuItem jPedido;
+    private javax.swing.JMenu jProductos;
+    private javax.swing.JMenu jReserva;
+    private javax.swing.JMenu jSalir;
+    // End of variables declaration//GEN-END:variables
+   
+    //INCORPORAR IMAGEN AL ESCRITORIO
+    public class EscritorioPersonalizado extends JDesktopPane{
+        private BufferedImage img;
+
+        public EscritorioPersonalizado() {
+            try{
+                img=ImageIO.read(getClass().getResourceAsStream("/Recursos/r20.png"));
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
+            g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+        }
+
+      }
+}
